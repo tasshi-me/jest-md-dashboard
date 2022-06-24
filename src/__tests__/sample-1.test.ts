a4dabe123b4f779d49890bc0a6abe29c2a2ad4b5
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-06-25 02:07:12 +0900

    test: create dummy tests

---
+++ src/__tests__/sample-1.test.ts
@@ -0,0 +1,47 @@
+describe("describe depth 1", () => {
+  test("test 1", async () => {
+    expect(1).toBe(1);
+  });
+  test("test 2", async () => {
+    expect(1).toBe(1);
+  });
+  describe("describe depth 2", () => {
+    test("test 1", async () => {
+      expect(1).toBe(1);
+    });
+    test("test 2", async () => {
+      expect(1).toBe(1);
+    });
+    describe("describe depth 3", () => {
+      test("test 1", async () => {
+        expect(1).toBe(1);
+      });
+      test("test 2", async () => {
+        expect(1).toBe(1);
+      });
+    });
+  });
+  describe("describe depth 2-2", () => {
+    test("test 1", async () => {
+      expect(1).toBe(1);
+    });
+    test("test 2", async () => {
+      expect(1).toBe(1);
+    });
+  });
+  test("test 3", async () => {
+    expect(1).toBe(1);
+  });
+  test("test 4", async () => {
+    expect(1).toBe(1);
+  });
+});
+
+describe("describe depth 1-2", () => {
+  test.each([1, 2])("parametarized: %d", (value) => {
+    expect(1).toBe(1);
+  });
+  test.each([1, 2])("parametarized: %d", (value) => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/__tests__/sample-2.test.ts
@@ -0,0 +1,14 @@
+describe("describe depth 1", () => {
+  test("test 1", async () => {
+    expect(1).toBe(1);
+  });
+  test("test 2", async () => {
+    expect(1).toBe(1);
+  });
+  test("test 3", async () => {
+    expect(1).toBe(1);
+  });
+  test("test 4", async () => {
+    expect(1).toBe(1);
+  });
+});
