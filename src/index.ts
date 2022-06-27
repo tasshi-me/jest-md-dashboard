725bfa0e8980b5f9a2d6273e47d439f25c2690c8
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-06-27 16:39:30 +0900

    separate object conversion and printing processes

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,11 @@ import type {
   Reporter,
   ReporterContext,
   ReporterOnStartOptions,
-  // eslint-disable-next-line node/no-unpublished-import
 } from "@jest/reporters";
 import type {
   AggregatedResult,
+  AssertionResult,
   TestContext,
-  // eslint-disable-next-line node/no-unpublished-import
 } from "@jest/test-result";
 // eslint-disable-next-line node/no-unpublished-import
 import type { Config } from "@jest/types";
@@ -48,28 +47,10 @@ export class MarkdownDashboardReporter implements Reporter {
     testContexts: Set<TestContext>,
     results: AggregatedResult
   ): Promise<void> {
-    let resultText = `# ${this.title}\n\n`;
-    for (const testResultsByFile of results.testResults) {
-      resultText += `## ${path.relative("", testResultsByFile.testFilePath)}\n`;
-      let lastAncestorTitles: string[] = [];
-      for (const testResult of testResultsByFile.testResults) {
-        for (const [
-          index,
-          ancestorTitle,
-        ] of testResult.ancestorTitles.entries()) {
-          if (
-            lastAncestorTitles.length < index ||
-            ancestorTitle !== lastAncestorTitles[index]
-          ) {
-            resultText += `${"  ".repeat(index)}- ${ancestorTitle}\n`;
-          }
-        }
-        lastAncestorTitles = testResult.ancestorTitles;
-        resultText += `${"  ".repeat(testResult.ancestorTitles.length)}- ${
-          testResult.title
-        }\n`;
-      }
-    }
+    const dashboard = this.convertResultsToDashboard(results, {
+      title: this.title,
+    });
+    const resultText = this.printDashBoard(dashboard);
     if (this.outputPath !== undefined && this.outputPath.length > 0) {
       await fs.mkdir(path.dirname(this.outputPath), { recursive: true });
       await fs.writeFile(this.outputPath, resultText);
@@ -77,6 +58,146 @@ export class MarkdownDashboardReporter implements Reporter {
       console.log(resultText);
     }
   }
+
+  convertResultsToDashboard(
+    results: AggregatedResult,
+    options: { title: string }
+  ): Dashboard {
+    const testFiles: TestFiles[] = [];
+    for (const testResultsByFile of results.testResults) {
+      const filePath = path.relative(
+        process.cwd(),
+        testResultsByFile.testFilePath
+      );
+      const describes = this.convertChildren(testResultsByFile.testResults, 0);
+      testFiles.push({ filePath, describes });
+    }
+    return {
+      title: options.title,
+      summary: "",
+      testFiles: testFiles,
+    };
+  }
+
+  // 現在のレイヤーについて、パースしてArray<Describe | Test>を返す
+  // resultsはcurrentLevelに対して同一のancestorTitlesを持つ
+  convertChildren(
+    results: AssertionResult[],
+    targetLevel: number
+  ): Array<Describe | Test> {
+    const children: Array<Describe | Test> = [];
+    for (let index = 0; index < results.length; index++) {
+      const result = results[index];
+      // console.log(" ".repeat(targetLevel), "------------------------");
+      // console.log(" ".repeat(targetLevel), "targetLevel: ", targetLevel);
+      // console.log(" ".repeat(targetLevel), "result:", result.title);
+      // console.log(
+      //   " ".repeat(targetLevel),
+      //   "ancestorTitles:",
+      //   result.ancestorTitles
+      // );
+      // console.log(" ".repeat(targetLevel), "index:", index);
+
+      // このLevel直下のテストの場合はテストを追加してcurrentIndexをインクリメントしてcontinueする
+      // If this is the test on this level
+      if (result.ancestorTitles.length === targetLevel) {
+        // console.log(" ".repeat(targetLevel), "this is Test");
+        children.push({
+          type: "test",
+          title: result.title,
+          status: result.status,
+        } as Test);
+        continue;
+      }
+
+      // If this is the test inside child describe
+
+      // 先頭の要素のdescribeを取得
+      // Pick the child ancestorTitle
+      const childAncestorTitle: string = result.ancestorTitles[targetLevel];
+      // console.log(" ".repeat(targetLevel), result.ancestorTitles[targetLevel]);
+      // 同じdescribeに入っているテストたちを取得
+      // console.log(
+      //   " ".repeat(targetLevel),
+      //   results.slice(index).map((r) => r.ancestorTitles.join())
+      // );
+      const lastIndex = results
+        .slice(index)
+        .findIndex((r) => r.ancestorTitles[targetLevel] !== childAncestorTitle);
+      // console.log(" ".repeat(targetLevel), "lastIndex: ", lastIndex);
+      const testsInThisDescribe =
+        lastIndex > 0
+          ? results.slice(index, index + lastIndex)
+          : results.slice(index);
+      // console.log(
+      //   " ".repeat(targetLevel),
+      //   "testsInThisDescribe.length: ",
+      //   testsInThisDescribe.length
+      // );
+      children.push({
+        type: "describe",
+        describe: result.ancestorTitles[targetLevel],
+        childs: this.convertChildren(testsInThisDescribe, targetLevel + 1),
+      });
+      index = lastIndex > 0 ? index + lastIndex - 1 : results.length;
+      // console.log(" ".repeat(targetLevel), "index after process", index);
+    }
+    return children;
+  }
+
+  printChildren(
+    children: Array<Test | Describe>,
+    currentLevel: number
+  ): string {
+    let resultText = "";
+    for (const child of children) {
+      switch (child.type) {
+        case "test":
+          resultText += `${"  ".repeat(currentLevel)}- ${child.title}\n`;
+          break;
+        case "describe":
+          resultText += `${"  ".repeat(currentLevel)}- ${child.describe}\n`;
+          resultText += this.printChildren(child.childs, currentLevel + 1);
+          break;
+        default:
+          throw new Error("Illegal state");
+      }
+    }
+    return resultText;
+  }
+
+  printDashBoard(dashboard: Dashboard): string {
+    let resultText = `# ${dashboard.title}\n\n`;
+    for (const file of dashboard.testFiles) {
+      resultText += `## ${file.filePath}\n`;
+      resultText += this.printChildren(file.describes, 0);
+    }
+
+    return resultText;
+  }
 }
+
+type Dashboard = {
+  title: string;
+  summary: string;
+  testFiles: TestFiles[];
+};
+
+type TestFiles = {
+  filePath: string;
+  describes: Array<Describe | Test>;
+};
+
+type Describe = {
+  type: "describe";
+  describe: string;
+  childs: Array<Describe | Test>;
+};
+
+type Test = {
+  type: "test";
+  title: string;
+  status: string;
+};
 
 export default MarkdownDashboardReporter;
