0b2558f5758ec45d942ff2f95ea981c192d95978
Masaharu TASHIRO <masatsr.kit@gmail.com>
2022-06-27 20:00:19 +0900

    refactor: extract dashboard related codes

---
+++ src/dashboard/printer.ts
@@ -0,0 +1,32 @@
+import { Dashboard, Describe, Test } from "./types.js";
+
+export const printDashBoard = (dashboard: Dashboard): string => {
+  let resultText = `# ${dashboard.title}\n\n`;
+  for (const file of dashboard.testFiles) {
+    resultText += `## ${file.filePath}\n`;
+    resultText += printChildren(file.children);
+  }
+
+  return resultText;
+};
+
+const printChildren = (
+  children: Array<Test | Describe>,
+  currentLevel: number = 0
+): string => {
+  let resultText = "";
+  for (const child of children) {
+    switch (child.type) {
+      case "test":
+        resultText += `${"  ".repeat(currentLevel)}- ${child.title}\n`;
+        break;
+      case "describe":
+        resultText += `${"  ".repeat(currentLevel)}- ${child.describe}\n`;
+        resultText += printChildren(child.children, currentLevel + 1);
+        break;
+      default:
+        throw new Error("Illegal state");
+    }
+  }
+  return resultText;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,14 @@
 import * as fs from "fs/promises";
 import path from "path";
 
-import type {
-  Reporter,
-  ReporterContext,
-  ReporterOnStartOptions,
-} from "@jest/reporters";
-import type {
-  AggregatedResult,
-  AssertionResult,
-  TestContext,
-} from "@jest/test-result";
-// eslint-disable-next-line node/no-unpublished-import
+import type { Reporter, ReporterContext } from "@jest/reporters";
+import type { AggregatedResult, TestContext } from "@jest/test-result";
 import type { Config } from "@jest/types";
+
+import {
+  convertResultsToDashboard,
+  printDashBoard,
+} from "./dashboard/index.js";
 
 export type ReporterOptions = {
   title?: string;
@@ -36,7 +32,7 @@ export class MarkdownDashboardReporter implements Reporter {
     this.context = reporterContext;
   }
 
-  onRunStart(results: AggregatedResult, options: ReporterOnStartOptions): void {
+  onRunStart() {
     // noop
   }
   getLastError() {
@@ -47,10 +43,11 @@ export class MarkdownDashboardReporter implements Reporter {
     testContexts: Set<TestContext>,
     results: AggregatedResult
   ): Promise<void> {
-    const dashboard = this.convertResultsToDashboard(results, {
+    const dashboard = convertResultsToDashboard(results, {
       title: this.title,
+      rootPath: process.cwd(),
     });
-    const resultText = this.printDashBoard(dashboard);
+    const resultText = printDashBoard(dashboard);
     if (this.outputPath !== undefined && this.outputPath.length > 0) {
       await fs.mkdir(path.dirname(this.outputPath), { recursive: true });
       await fs.writeFile(this.outputPath, resultText);
@@ -58,146 +55,6 @@ export class MarkdownDashboardReporter implements Reporter {
       console.log(resultText);
     }
   }
-
-  convertResultsToDashboard(
-    results: AggregatedResult,
-    options: { title: string }
-  ): Dashboard {
-    const testFiles: TestFiles[] = [];
-    for (const testResultsByFile of results.testResults) {
-      const filePath = path.relative(
-        process.cwd(),
-        testResultsByFile.testFilePath
-      );
-      const describes = this.convertChildren(testResultsByFile.testResults, 0);
-      testFiles.push({ filePath, describes });
-    }
-    return {
-      title: options.title,
-      summary: "",
-      testFiles: testFiles,
-    };
-  }
-
-  // 現在のレイヤーについて、パースしてArray<Describe | Test>を返す
-  // resultsはcurrentLevelに対して同一のancestorTitlesを持つ
-  convertChildren(
-    results: AssertionResult[],
-    targetLevel: number
-  ): Array<Describe | Test> {
-    const children: Array<Describe | Test> = [];
-    for (let index = 0; index < results.length; index++) {
-      const result = results[index];
-      // console.log(" ".repeat(targetLevel), "------------------------");
-      // console.log(" ".repeat(targetLevel), "targetLevel: ", targetLevel);
-      // console.log(" ".repeat(targetLevel), "result:", result.title);
-      // console.log(
-      //   " ".repeat(targetLevel),
-      //   "ancestorTitles:",
-      //   result.ancestorTitles
-      // );
-      // console.log(" ".repeat(targetLevel), "index:", index);
-
-      // このLevel直下のテストの場合はテストを追加してcurrentIndexをインクリメントしてcontinueする
-      // If this is the test on this level
-      if (result.ancestorTitles.length === targetLevel) {
-        // console.log(" ".repeat(targetLevel), "this is Test");
-        children.push({
-          type: "test",
-          title: result.title,
-          status: result.status,
-        } as Test);
-        continue;
-      }
-
-      // If this is the test inside child describe
-
-      // 先頭の要素のdescribeを取得
-      // Pick the child ancestorTitle
-      const childAncestorTitle: string = result.ancestorTitles[targetLevel];
-      // console.log(" ".repeat(targetLevel), result.ancestorTitles[targetLevel]);
-      // 同じdescribeに入っているテストたちを取得
-      // console.log(
-      //   " ".repeat(targetLevel),
-      //   results.slice(index).map((r) => r.ancestorTitles.join())
-      // );
-      const lastIndex = results
-        .slice(index)
-        .findIndex((r) => r.ancestorTitles[targetLevel] !== childAncestorTitle);
-      // console.log(" ".repeat(targetLevel), "lastIndex: ", lastIndex);
-      const testsInThisDescribe =
-        lastIndex > 0
-          ? results.slice(index, index + lastIndex)
-          : results.slice(index);
-      // console.log(
-      //   " ".repeat(targetLevel),
-      //   "testsInThisDescribe.length: ",
-      //   testsInThisDescribe.length
-      // );
-      children.push({
-        type: "describe",
-        describe: result.ancestorTitles[targetLevel],
-        childs: this.convertChildren(testsInThisDescribe, targetLevel + 1),
-      });
-      index = lastIndex > 0 ? index + lastIndex - 1 : results.length;
-      // console.log(" ".repeat(targetLevel), "index after process", index);
-    }
-    return children;
-  }
-
-  printChildren(
-    children: Array<Test | Describe>,
-    currentLevel: number
-  ): string {
-    let resultText = "";
-    for (const child of children) {
-      switch (child.type) {
-        case "test":
-          resultText += `${"  ".repeat(currentLevel)}- ${child.title}\n`;
-          break;
-        case "describe":
-          resultText += `${"  ".repeat(currentLevel)}- ${child.describe}\n`;
-          resultText += this.printChildren(child.childs, currentLevel + 1);
-          break;
-        default:
-          throw new Error("Illegal state");
-      }
-    }
-    return resultText;
-  }
-
-  printDashBoard(dashboard: Dashboard): string {
-    let resultText = `# ${dashboard.title}\n\n`;
-    for (const file of dashboard.testFiles) {
-      resultText += `## ${file.filePath}\n`;
-      resultText += this.printChildren(file.describes, 0);
-    }
-
-    return resultText;
-  }
 }
-
-type Dashboard = {
-  title: string;
-  summary: string;
-  testFiles: TestFiles[];
-};
-
-type TestFiles = {
-  filePath: string;
-  describes: Array<Describe | Test>;
-};
-
-type Describe = {
-  type: "describe";
-  describe: string;
-  childs: Array<Describe | Test>;
-};
-
-type Test = {
-  type: "test";
-  title: string;
-  status: string;
-};
 
 export default MarkdownDashboardReporter;
